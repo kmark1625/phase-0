a9d42768ba5b723643fb226640a7d0ec9bcd4bc2
Kevin Mark <kmark1625@gmail.com>
2015-10-04 15:29:15 -0500

    Add factorial exercise

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,15 +1,28 @@
 # Factorial
 
-# I worked on this challenge by myself.
+# I worked on this challenge with Jessie Richardson.
 
 
 # Your Solution Below
 def factorial(number)
-  if number == 1
-    return 1
+      # =begin
+      # Pseudo-code here
+      # Only accepts a positive number
+      # Take number and calculate factorial
+      # while number >= 0
+      #   number*(number-1)*(number-2)*...(1)
+      # outputs result
+      # =end
+
+  if number < 0
+    return None
   end
-  if number == 0
-    return 1
+  result = 1
+  while number > 1
+    result = result * number
+    number = number - 1
   end
-  return number*factorial(number-1)
+  return result
 end
+
+
