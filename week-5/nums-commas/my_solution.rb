9d36dd5aca81c7830ecd95938cd67c26efb5ec71
Kevin Mark <kmark1625@gmail.com>
2015-10-05 20:27:15 -0500

    Add reflection.

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -57,9 +57,13 @@ end
 # 3. Reflection
 =begin
 * What was your process for breaking the problem down? What different approaches did you consider?
-  ** My initial process was to loop through each character in the integer and add
+  ** My initial process was to loop through each character in the integer and add a comma where appropriate.  I considered trying to use some sort of "join" method but abandoned the idea because I wanted the initial solution to be without more than basic functions.
 * What was your pseudocode effective in helping you build a successful initial solution?
+  ** My pseudocode was effective in helping me build a successful initial solution.  I did have to slightly revise the ordering of my pseudocode in the process of actually writing the code.
 * What Ruby method(s) did you use when refactoring your solution? What difficulties did you have implementing it/them? Did it/they significantly change the way your code works? If so, how?
+  ** I used the scan method to refactor my solution. It was difficult to use the scan method directly and instead I had to come up with another function to pad the string appropriately.  It significantly changes the way my code works as it no longer loops across each character and instead combines groups of characters.
 * How did you initially iterate through the data structure?
+  ** Initially I converted the int to a string and looped through each character in the string.  Later, I split the string into a list of groups of 3 characters and then I combine them back into a string with a delimeter.
 * Do you feel your refactored solution is more readable than your initial solution? Why?
+  ** I feel my refactored solution is much easier to read as it breaks it up into logical functions.
 =end
