7ab0ad0929a6d85b1bc9e895f1ded0cb16fbcfa5
Kevin Mark <kmark1625@gmail.com>
2015-10-14 17:31:43 -0500

    Add bingo solution.

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -70,7 +70,35 @@ class BingoBoard
 end
 
 # Refactored Solution
+class BingoBoard
 
+  def initialize(board)
+    @letters = ['B','I','N','G','O']
+    @bingo_board = board
+  end
+
+  def call # Method returns a letter and a number
+    letter = @letters[Random.rand(5)]
+    number = Random.rand(99) + 1
+    return letter, number
+  end
+
+  def check(letter, number) # Replace number with string
+    col = @letters.find_index(letter)
+    @bingo_board.each do |row|
+        row[col] = 'X' if row[col] == number
+    end
+  end
+
+  def display() # displays the board
+    @bingo_board.each do |row|
+      row.each do |item|
+        printf("%2s ",item.to_s)
+      end
+      puts
+    end
+  end
+end
 
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
@@ -86,4 +114,29 @@ new_game.check('B', 47)
 new_game.display
 
 #Reflection
-
+=begin
+* How difficult was pseudocoding this challenge? What do you think of
+your pseudocoding style?
+  ** It was difficult for me to pseudo-code this challenge at times
+  because I thought certain methods were easier to code in Ruby than
+  they were in pseudo-code. I just need more practice with pseudo-code
+  so that my style can be more clear/concise.
+* What are the benefits of using a class for this challenge?
+  ** Using a class allows you to keep all of the methods/variables that
+  deal with the class inside the object implementation. This helps with
+  separation of concerns. It is also good object oriented design.
+* How can you access coordinates in a nested array?
+  ** You can access coordinates in an a nested array using to sets of
+  indices such as nested_array[x][y].
+* What methods did you use to access and modify the array?
+  ** I used each loops to access and modify the contents of the array.
+  I also referenced the items via indices.
+* How did you determine what should be an instance variable versus a
+local variable?
+  ** All variables that dealt with the state of the bingo board were
+  created as instance variables.
+* What do you feel is most improved in your refactored solution?
+  **The refactored solution is much easier to read. The greatest
+  impact was in the display function where I was able to make the
+  formatting nicer with printf rather than gibberish print statements.
+=end
